9812f8eeace9f0c09b662b0ce5cf2f6720694909
saad <professeurgamingyt@gmail.com>
2022-03-08 20:03:24 +0100

    Commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,52 +1,44 @@
 <template>
-    <Layout>
-        <div v-if="!$store.state.user.auth" class="d-flex justify-content-center">
-            <div class="col-12 col-md-10 col-lg-8 col-xl-5">
-                <div class="form-group mb-3">
-                    <div class="d-flex justify-content-center">
-                        <div class="footer_logo"><a>{{$hostname}}</a></div>
-                    </div>
+       <div class="container">
+    <div class="row d-flex justify-content-center mt-5">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card py-3 px-2">
+                <p class="text-center mb-3 mt-2">SE CONNECTER AVEC</p>
+                <div class="row mx-auto ">
+                    <div class="col-4"> <i class="fab fa-twitter"></i> </div>
+                    <div class="col-4"> <i class="fab fa-facebook"></i> </div>
+                    <div class="col-4"> <i class="fab fa-google"></i> </div>
                 </div>
-                <div class="form-group" v-if="alert">
-                    <div class="alert alert-danger">
-                        <span>{{alert}}</span>
-                    </div>
-                </div>
-                <Spinner v-if="loading"></Spinner>
-                <div v-else>
-                    <div class="form-group row align-items-center mb-3">
-                        <div class="d-flex justify-content-start">
-                            <label for="email">Email: </label>
+                <div class="division">
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="line l"></div>
                         </div>
-                        <div>
-                            <input type="text" v-model="email" id="email" placeholder="Email">
-                        </div>
-                    </div>
-                    <div class="form-group row align-items-center mb-3">
-                        <div class="d-flex justify-content-start">
-                            <label for="password">Password: </label>
-                        </div>
-                        <div>
-                            <input type="password" v-model="password" id="password" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="d-flex justify-content-end">
-                            <button class="btn btn-ligth border-2 border-dark px-5 rounded-0" @click="login()"><span>Login</span></button>
+                        <div class="col-6"><span>OU AVEC MON EMAIL</span></div>
+                        <div class="col-3">
+                            <div class="line r"></div>
                         </div>
                     </div>
                 </div>
+                <form class="myform">
+                    <div class="form-group"> <input type="email" class="form-control" placeholder="Email"> </div>
+                    <div class="form-group"> <input type="password" class="form-control" placeholder="Mot de passe"> </div>
+                    <div class="row">
+                        <div class="col-md-6 col-12">
+                            <div class="form-group form-check"> <input type="checkbox" class="form-check-input" id="exampleCheck1"> <label class="form-check-label" for="exampleCheck1">Rester connecte</label> </div>
+                        </div>
+                        <div class="col-md-6 col-12 bn">Mot se passe oublie</div>
+                    </div>
+                    <div class="form-group mt-3"> <button type="button" class="btn btn-block btn-primary btn-lg"><small><i class="far fa-user pr-2"></i>Se connecter</small></button> </div>
+                </form>
             </div>
         </div>
-    </Layout>
+    </div>
+</div>
 </template>
 
 <script>
 import axios from 'axios'
-
-import Layout from '@/components/Layout'
-import Spinner from '@/components/Spinner.vue'
-
 export default {
     data(){
         return {
@@ -57,10 +49,6 @@ export default {
         }
     },
     name: 'Login',
-    components: {
-        Layout,
-        Spinner,
-    },
     methods: {
         login: function(){
             let self = this
@@ -94,13 +82,155 @@ export default {
     },
 }
 </script>
-<style scoped>
-input{
-    width: 100%;
-    height: 52px;
-    background: #f2f4f8;
+
+
+<style>
+
+.card {
     border: none;
-    outline: none;
-    padding-left: 15px;
+    border-top: 5px solid rgb(176, 106, 252);
+    background: #212042;
+    color: #57557A
+}
+
+p {
+    font-weight: 600;
+    font-size: 15px
+}
+
+.fab {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: #2A284D;
+    height: 40px;
+    width: 90px
+}
+
+.fab:hover {
+    cursor: pointer
+}
+
+.fa-twitter {
+    color: #56ABEC
+}
+
+.fa-facebook {
+    color: #1775F1
+}
+
+.fa-google {
+    color: #CB5048
+}
+
+.division {
+    float: none;
+    position: relative;
+    margin: 30px auto 20px;
+    text-align: center;
+    width: 100%;
+    box-sizing: border-box
+}
+
+.division .line {
+    border-top: 1.5px solid #57557A;
+    ;
+    position: absolute;
+    top: 13px;
+    width: 85%
+}
+
+.line.l {
+    left: 52px
+}
+
+.line.r {
+    right: 45px
+}
+
+.division span {
+    font-weight: 600;
+    font-size: 14px
+}
+
+.myform {
+    padding: 0 25px 0 33px
+}
+
+.form-control {
+    border: 1px solid #57557A;
+    border-radius: 3px;
+    background: #212042;
+    margin-bottom: 20px;
+    letter-spacing: 1px
+}
+
+.form-control:focus {
+    border: 1px solid #57557A;
+    border-radius: 3px;
+    box-shadow: none;
+    background: #212042;
+    color: #fff;
+    letter-spacing: 1px
+}
+
+.bn {
+    text-decoration: underline
+}
+
+.bn:hover {
+    cursor: pointer
+}
+
+.form-check-input {
+    margin-top: 8px !important
+}
+
+.btn-primary {
+    background: linear-gradient(135deg, rgba(176, 106, 252, 1) 39%, rgba(116, 17, 255, 1) 101%);
+    border: none;
+    border-radius: 50px
+}
+
+.btn-primary:focus {
+    box-shadow: none;
+    border: none
+}
+
+small {
+    color: #F2CEFF
+}
+
+.far.fa-user {
+    font-size: 13px
+}
+
+@media(min-width: 767px) {
+    .bn {
+        text-align: right
+    }
+}
+
+@media(max-width: 767px) {
+    .form-check {
+        text-align: center
+    }
+
+    .bn {
+        text-align: center;
+        align-items: center
+    }
+}
+
+@media(max-width: 450px) {
+    .fab {
+        width: 100%;
+        height: 100%
+    }
+
+    .division .line {
+        width: 50%
+    }
 }
 </style>
